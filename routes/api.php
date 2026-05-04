6cf6cb6c7b1a23f86db898d85cc711a645e8b1dd
wardDward <edward.taligatos03@gmail.com>
2026-05-04 13:33:52 +0800

    feat: install api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+require_once('api/auth.php');
