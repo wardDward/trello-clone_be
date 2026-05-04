6b24527f16f85bb28595386b60db699ba89a59ce
wardDward <edward.taligatos03@gmail.com>
2026-05-04 10:37:19 +0800

    feat: mysql prefix for table

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
+            'prefix' => 'tbl_',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
